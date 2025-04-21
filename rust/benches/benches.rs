87a59d79dc9f79198d4c18906dc01be2644f565c
Ross MacArthur <ross@macarthur.io>
2025-04-21 20:04:21 +0200

    rust: Add simple benchmark

---
+++ rust/benches/benches.rs
@@ -0,0 +1,23 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+
+criterion_main! { benches }
+
+criterion_group! { benches, bench_simple }
+
+pub fn bench_simple(c: &mut Criterion) {
+    let mut g = c.benchmark_group("simple");
+
+    let input = "thisIsACamelCaseString".repeat(1000);
+    let expect = "this_is_a_camel_case_string".repeat(1000);
+
+    assert_eq!(anycase::to_snake(&input), expect);
+    assert_eq!(anycase::fmt::snake(&input).to_string(), expect);
+
+    g.bench_with_input("string", &input, |b, input| {
+        b.iter(|| anycase::to_snake(input));
+    });
+
+    g.bench_with_input("fmt", &input, |b, input| {
+        b.iter(|| anycase::fmt::snake(input).to_string());
+    });
+}
